bf58b67b2f80e213552f5e2dc9abe20d4fabe1a3
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-22 20:04:01 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,28 @@
+variable "aws_region" {
+  description = "The AWS region to create our infrastructure"
+  default     = "us-east-2"
+}
+
+variable "AZ_1" {
+  description = "Availability zone 1"
+  default = "use2-az1"
+}
+
+variable "AZ_2" {
+  description = "Availability zone 2"
+  default = "use2-az2"
+}
+
+variable "ec2_type" {
+  description = "The type of ec2 instances to create"
+  default = "t2.micro"
+}
+
+variable "ec2_ami" {
+  description = "The ami image to use for ec2 instances"
+  default = "ami-077e31c4939f6a2f3"
+}
+
 variable "access_key" {
   type        = string
   default     = ""
@@ -8,9 +33,29 @@ variable "secret_key" {
   default     = ""
 }
 
-variable "eks_cluster_name" {
-  type        = string
-  default     = "my-eks-kluster"
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  default = "192.168.0.0/16"
+}
+
+variable "public_subnet_1_CIDR" {
+  description = "Public Subnet AZ 1 CIDR"
+  default = "192.168.1.0/24"
+}
+
+variable "public_subnet_2_CIDR" {
+  description = "Public Subnet AZ 1 CIDR"
+  default = "192.168.2.0/24"
+}
+
+variable "private_subnet_1_CIDR" {
+  description = "Private Subnet AZ 1 CIDR"
+  default = "192.168.10.0/24"
+}
+
+variable "private_subnet_2_CIDR" {
+  description = "Private Subnet AZ 1 CIDR"
+  default = "192.168.20.0/24"
 }
 
 variable "public_key_name" {
