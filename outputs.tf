9c1f6327d818f0cf0cc50f106bc05e2a85918c94
broadbounds <84296908+broadbounds@users.noreply.github.com>
2021-07-23 20:31:01 -0700

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,4 @@
+  Bastion host 1 public ip address: ${aws_eip.bastion_elastic_ip_1.public_ip}
+  Bastion host 1 private ip address: ${aws_instance.bastion_host_1.private_ip}
+  Bastion host 2 public ip address: ${aws_eip.bastion_elastic_ip_2.public_ip}
+  Bastion host 2 private ip address: ${aws_instance.bastion_host_2.private_ip}
